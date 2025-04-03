198f305ca965cf09779ecbdef641d6bc2f1acb55
Kaung Htet San <kghtetsan26@gmail.com>
2025-04-03 10:15:32 +0630

    install material tailwind library and setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+import { mtConfig } from "@material-tailwind/react";
 
+/** @type {import('tailwindcss').Config} */
 export default {
 
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [mtConfig],
 }
