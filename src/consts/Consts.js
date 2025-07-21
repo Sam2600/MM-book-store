85efd4adf6456c338a7265678fd421894769d671
Kaung Htet San <kghtetsan26@gmail.com>
2025-07-22 00:31:34 +0630

    feat(consts): add constants for navigation links and upload menu

---
+++ src/consts/Consts.js
@@ -0,0 +1,23 @@
+export const LINKS = [
+   {
+      icon: "MultiplePages",
+      title: "သိမ်းထားသည့် စာအုပ်များ",
+      href: "/my-books",
+   },
+   {
+      icon: "ProfileCircle",
+      title: "အကောင့်",
+      href: "/account",
+   },
+   {
+      icon: "SelectFace3d",
+      title: "စာဖတ်သူ လမ်းညွှန်",
+      href: "/guides",
+   },
+];
+
+export const UPLOAD_MENU = {
+   icon: "CloudUpload",
+   title: "စာတင်မယ်",
+   href: "/upload-chapters",
+};
--- src/functions/helpers.js
+++ src/functions/helpers.js
@@ -1,3 +1,10 @@
+import {
+   MultiplePages,
+   ProfileCircle,
+   SelectFace3d,
+   CloudUpload,
+} from "iconoir-react";
+
 export const getApiPath = () => import.meta.env.VITE_API_BASE_URL;
 
 export const toHumanReadableDates = (timestamp) => {
@@ -6,11 +13,25 @@ export const toHumanReadableDates = (timestamp) => {
 
    // Format it to a human-readable string
    const humanReadable = date.toLocaleString('en-US', {
-      weekday: 'long',    // Day of the week (e.g., "Saturday")
+      weekday: 'short',    // Day of the week (e.g., "Saturday")
       year: 'numeric',    // Full year (e.g., "2025")
-      month: 'long',      // Full month (e.g., "April")
+      month: 'short',      // Full month (e.g., "April")
       day: 'numeric',     // Day of the month (e.g., "12")
    });
 
    return humanReadable;
 }
+
+export const scrollToTop = () => {
+   window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+   });
+}
+
+export const iconMap = {
+   MultiplePages,
+   ProfileCircle,
+   SelectFace3d,
+   CloudUpload,
+};
