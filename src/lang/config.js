fee7e746540895235efe63ca3542bec6e39bdfde
Kaung Htet San <kghtetsan26@gmail.com>
2025-07-22 22:10:51 +0630

    fix(config): disable debug mode for production environment

--- src/lang/config.js
+++ src/lang/config.js
@@ -22,7 +22,7 @@ i18n
 
       // Enables useful output in the browser’s
       // dev console.
-      debug: true,
+      debug: false,
 
       // Normally, we want `escapeValue: true` as it
       // ensures that i18next escapes any code in
